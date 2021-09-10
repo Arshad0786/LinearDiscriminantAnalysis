7cece30fbbcaaed90c1c7356e940b08025bd6ace
Arshad520786 <js07150715@gmail.com>
2021-09-10 14:32:30 +0800

    fixed some errors during matrix calulation

--- LinearDiscriminantAnalysis.py
+++ LinearDiscriminantAnalysis.py
@@ -7,20 +7,20 @@ iris = pandas.read_csv("C:\\Users\\js071\\Downloads\\iris.data.csv", names=[
 
 
 # create color dictionary
-colors = {'Iris-setosa': 'r', 'Iris-versicolor': 'g', 'Iris-virginica': 'b'}
+colors = {'Iris-setosa': 'r', 'Iris-versicolor': 'g', 'Iris-virginica': 'g'}
 # create a figure and axis
 fig, ax = plt.subplots()
 # plot each data-point
-"""
-for i in range(100):
+
+for i in range(150):
     ax.scatter(iris['sepal_length'][i], iris['sepal_width']
                [i], color=colors[iris['class'][i]])
-"""
+
 
 Iris_setosa_list = list()
 Iris_versicolor_list = list()
 
-for i in range(100):  # transform attributes from pandas dataframe to list
+for i in range(150):  # transform attributes from pandas dataframe to list
     if (i < 50):
         Iris_setosa_list.append(
             (iris['sepal_length'][i], iris['sepal_width'][i]))
@@ -35,7 +35,7 @@ def isListDataCorrect():  # check if data is align with original data
             return False
         if(Iris_setosa_list[i][1] != iris['sepal_width'][i]):
             return False
-    for i in range(50, 100):
+    for i in range(50, 150):
         if(Iris_versicolor_list[i-50][0] != iris['sepal_length'][i]):
             return False
         if(Iris_versicolor_list[i-50][1] != iris['sepal_width'][i]):
@@ -60,23 +60,25 @@ Iris_versicolor_mean = mean(Iris_versicolor_list)
 # find B
 u1 = np.array([Iris_setosa_mean])
 u2 = np.array([Iris_versicolor_mean])
-B = u1-u2*np.transpose(u1-u2)
-
+B = np.multiply(np.transpose(u1-u2), u1-u2)
 # calculate scatter
 S1 = np.array([[0, 0], [0, 0]])
 S2 = np.array([[0, 0], [0, 0]])
-for i in range(50):
-    S1 = S1 + (np.array(Iris_versicolor_list[i]) - u1) * \
-        np.transpose((np.array(Iris_versicolor_list[i]) - u1))
-    S2 = S2 + (np.array(Iris_setosa_list[i]) - u1) * \
+
+for i in range(len(Iris_setosa_list)):
+    S1 = S1 + (np.array(Iris_setosa_list[i]) - u1) * \
         np.transpose((np.array(Iris_setosa_list[i]) - u2))
+
+for i in range(len(Iris_versicolor_list)):
+    S2 = S2 + (np.array(Iris_versicolor_list[i]) - u2) * \
+        np.transpose((np.array(Iris_versicolor_list[i]) - u1))
+
 S = S1 + S2
-print(S2)
 
 objecitveMatrix = np.matmul((np.linalg.inv(S)), B)  # find (S^-1)*B
 eigval, eigvec = np.linalg.eig(objecitveMatrix)  # then we find it's eigenvalue
 # find eigenvector correspond to the maximal eigenvalue
-MaxEigvec = eigvec[np.argmax(eigval)]
+MaxEigvec = eigvec[:,np.argmax(eigval)]
 # make it unit vector so it's easier to find projection matrix
 MaxEigvec = MaxEigvec / np.linalg.norm(MaxEigvec)
 # make it 1x2 cause so numpy can transpose it
@@ -84,12 +86,14 @@ MaxEigvec = np.array([MaxEigvec])
 # find projection matrix of that eigen vector
 Projection_matrix = np.transpose(MaxEigvec)*MaxEigvec
 
+
 projected_Iris_setosa = list()
 projected_Iris_versicolor = list()
 
-for i in range(50):  # find all points
+for i in range(len(Iris_setosa_list)):  # find all points
     projected_Iris_setosa.append(
         np.matmul(Projection_matrix, np.transpose(np.array([Iris_setosa_list[i]]))))
+for i in range(len(Iris_versicolor_list)):
     projected_Iris_versicolor.append(
         np.matmul(Projection_matrix, np.transpose(np.array([Iris_versicolor_list[i]]))))
 
@@ -100,7 +104,7 @@ result = np.matmul(Projection_matrix, np.transpose(
 Pmean1 = mean(projected_Iris_setosa)
 Pmean2 = mean(projected_Iris_versicolor)
 
-for i in range(100):
+for i in range(150):
     #print(i, " : ", JoinedList[i])
     if (i < 50):
         ax.scatter(JoinedList[i][0], JoinedList[i][1], color='r')
